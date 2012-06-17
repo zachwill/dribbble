14b3c8a2aa36016fe5a0f1b23ccd2a768f819c31
Zach Williams <hey@zachwill.com>
2012-06-17 14:16:29 -0700

    Update the settings.py file

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="dribbble",
+    version="0.1",
+    description="An easy-to-use Python wrapper for the Dribbble API.",
+    author="Zach Williams",
+    author_email="hey@zachwill.com",
+    url="http://github.com/zachwill/dribbble",
+    license="MIT",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+    ],
+    packages=[
+        "dribbble"
+    ],
+    install_requires=[
+        "mock",
+        "lxml",
+        "requests",
+        "simplejson"
+    ]
+)

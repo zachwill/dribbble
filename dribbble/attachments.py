01fee9aa922d0ba9aa5bdd946f18cf9d373d7edb
Zach Williams <hey@zachwill.com>
2012-06-17 14:10:02 -0700

    :zap: It works :zap:

--- dribbble/attachments.py
+++ dribbble/attachments.py
@@ -3,22 +3,29 @@ Check for attachments.
 """
 
 import requests as req
-from lxml.cssselect import CSSSelector as css
-from lxml.etree import fromstring
+from lxml.cssselect import CSSSelector
+from lxml.html import fromstring as parse
 
 
-selector = css('.attachments a')
+css = CSSSelector('.attachments a')
 
 
 def attachments(shot):
     """Check if there are PSD attachments for a given shot."""
     endpoint = "http://dribbble.com/shots/%s" % str(shot)
     content = req.get(endpoint).content
-    html = fromstring(content)
-    links = psds(selector(html))
-    return links
+    html = parse(content)
+    return psds(html)
 
 
-def psds(attachments):
+def psds(html):
     """Return PSD links."""
-    return attachments
+    files = []
+    links = css(html)
+    for link in links:
+        name = link.text.lower()
+        if name.endswith('psd'):
+            href = link.get('href')
+            full_url = "http://dribble.com" + href
+            files.append(full_url)
+    return files
